002a6f9b8a3b3f512d582fd019a8085ea05f155f
fardkrsnaa <kangparid73@gmail.com>
2026-06-15 17:58:15 +0700

    Update PosPage Kasir Kedai Ungu

--- src/pages/PosPage.tsx
+++ src/pages/PosPage.tsx
@@ -327,7 +327,6 @@ export default function PosPage() {
             value={search}
             onChange={(e) => setSearch(e.target.value)}
             className="w-full pl-10 pr-4 py-2.5 bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-primary-500 dark:text-white"
-            autoFocus
           />
         </div>
 
